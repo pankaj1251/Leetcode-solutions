d4ce5dc9b57415560438d562e8c19f697772e8a9
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-05-26 11:50:03 +0530

    Time: 33 ms (23.82%), Space: 16.3 MB (28.73%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int func(vector<int>& piles, int i, int j, bool turn){
+    int func(vector<int>& piles, int i, int j, bool turn, vector<vector<int>>&dp){
         if(i>j)return 0;
-
+        if(dp[i][j]!=-1)return dp[i][j];
         if(turn){
-            int t1 = func(piles, i+1, j, false) + piles[i];
-            int t2 = func(piles, i, j-1, false) + piles[j];
+            int t1 = func(piles, i+1, j, false, dp) + piles[i];
+            int t2 = func(piles, i, j-1, false, dp) + piles[j];
 
-            return max(t1, t2);
+            return dp[i][j] = max(t1, t2);
         }
 
         else{
-            int t1 = func(piles, i+1, j, true) - piles[i];
-            int t2 = func(piles, i, j-1, true) - piles[j];
+            int t1 = func(piles, i+1, j, true, dp) - piles[i];
+            int t2 = func(piles, i, j-1, true, dp) - piles[j];
 
-            return min(t1, t2);
+            return dp[i][j] = min(t1, t2);
         }
     }
 
     bool stoneGame(vector<int>& piles) {
-        return(piles, 0, piles.size()-1, true);
+        int n = piles.size();
+        vector<vector<int>>dp(n, vector<int>(n, -1));
+        return func(piles, 0, n-1, true, dp);
     }
 };
