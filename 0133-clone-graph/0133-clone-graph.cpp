88db01c188db4817ff415d3cd785ec623ca9dfd2
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-06-11 13:29:19 +0530

    Time: 7 ms (44.98%), Space: 8.6 MB (83.01%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,51 +1,37 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
 public:
-    unordered_map<Node*, Node*>umap;
-
+    unordered_map<Node* , Node*> mp; // declaring map, to check whwther we have a copy of node or not and also to store copy
+    
     Node* cloneGraph(Node* node) {
-        if(node==NULL)return NULL;
-
-        Node *ans = new Node(node->val, {});
-
-        umap[node] = ans;
-
-        queue<Node*>Q;
-        Q.push(node);
-
-        while(!Q.empty()){
-            Node* temp = Q.front();
-            Q.pop();
-
-            for(auto it: temp->neighbors){
-                if(umap.find(it) == umap.end()){
-                    umap[it] = new Node(it->val, {});
-                    Q.push(it);
+        if(node == NULL) // if node is null, then simply return null
+        {
+            return NULL;
+        }
+        
+        Node* first = new Node(node -> val, {}); // make a copy of first node
+        mp[node] = first;
+        
+        queue<Node*> q; //For bfs, we create queue
+        q.push(node); // push into queue
+        
+        while(q.empty() == false) // until q. empty == false
+        {
+            Node* curr = q.front(); // extract front node
+            q.pop(); // pop that from queue
+            
+            for(auto adj: curr -> neighbors) // now travel in adjcant
+            {
+                if(mp.find(adj) == mp.end()) // if not present in map
+                {
+                    mp[adj] = new Node(adj -> val, {}); // then create copy
+                    q.push(adj); // push nto the queue
+                    
                 }
-                umap[temp]->neighbors.push_back(umap[it]); 
+                
+                mp[curr] -> neighbors.push_back(mp[adj]); // in current node push adjcant node
             }
         }
-
-        return ans;
+        
+        return mp[node];
     }
 };
