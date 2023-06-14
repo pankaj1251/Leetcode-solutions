4f69090f180a971576faee0589916ad3bec8808e
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-06-14 10:13:52 +0530

    Time: 232 ms (5%), Space: 56 MB (5.03%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void func(vector<int>& arr, int idx, int target, vector<int>&path, vector<vector<int>>&ans){
-
+    void func(vector<int>& arr, int i, int target, int prev, vector<int>path, set<vector<int>>&ans){
         if(target==0){
-            ans.push_back(path);
+            ans.insert(path);
             return;
         }
 
-        for(int i=idx; i<arr.size(); i++){
-            if(i>idx and arr[i]==arr[i-1])continue;
-            if(arr[i]>target)break;
+        if(target<0 || i>=arr.size())return; 
 
-            path.push_back(arr[i]);
-            func(arr, i+1, target-arr[i], path, ans);
-            path.pop_back();
-        }
+        func(arr, i+1, target, target, path, ans);
+        if(i>0 and arr[i-1]==arr[i] and prev==target)return;
+        path.push_back(arr[i]);
+        func(arr, i+1, target-arr[i], target, path, ans);
+        path.pop_back();
     }
 
     vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
         vector<vector<int>>ans;
         int n = arr.size();
+        set<vector<int>>res;
         vector<int>path;
-        int idx=0;
         sort(arr.begin(), arr.end());
-        func(arr, idx, target, path, ans);
+        func(arr, 0, target, target, path, res);
 
+        for(auto &it: res)ans.push_back(it);
 
         return ans;
     }
