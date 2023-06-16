001b3214e64d490a1bcac92b709a66d7c514d878
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-06-16 11:10:17 +0530

    Time: 0 ms (100%), Space: 16.4 MB (20.56%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void func(vector<int>& arr, int idx, set<vector<int>>&ans, vector<int>subset)
+    {
+        ans.insert(subset);
+
+        if(idx==arr.size()){
+            ans.insert(subset);
+            return;
+        }
+
+        subset.push_back(arr[idx]);
+
+        func(arr, idx+1, ans, subset);
+
+        subset.pop_back();
+
+        func(arr, idx+1, ans, subset);
+    }
+    vector<vector<int>> subsets(vector<int>& arr) {
+        vector<vector<int>>ans;
+        set<vector<int>>subsets;
+        vector<int>st;
+        func(arr, 0, subsets, st);
+
+        for(auto it: subsets){
+            ans.push_back(it);
+        }
+
+        return ans;
+    }
+};
