3499f6712b02bc915f6e7e01174fbefd6cd9b29a
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-06-22 14:14:19 +0530

    Time: 24 ms (31.22%), Space: 13.5 MB (30.76%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool func(string &s, int idx, set<string>&st, vector<int>&dp){
+        if(idx==s.length())return true;
+        if(dp[idx] != -1)return false;
+
+        for(int i=idx; i<s.length(); i++){
+            string temp = s.substr(idx, i-idx+1);
+            if(st.find(temp) != st.end() and func(s, i+1, st, dp)){
+                return dp[idx] = true;
+            }
+        }
+        return dp[idx] = false;
+    }
+
+    bool wordBreak(string s, vector<string>& words) 
+    {
+        set<string>st;
+        vector<int>dp(s.size(), -1);
+        for(auto it: words)st.insert(it);
+        return func(s, 0, st, dp);
+    }
+};
