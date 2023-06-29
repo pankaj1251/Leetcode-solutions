f3292a94cf07faee6b948fa77518c4b65de348c5
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-06-29 21:05:23 +0530

    Time: 28 ms (83.11%), Space: 24.2 MB (10.86%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    int idx;
+    vector<int>arr;
+    BSTIterator(TreeNode* root) {
+        idx=0;
+        inorder(root);
+    }
+
+    void inorder(TreeNode* root){
+        if(root==NULL)return;
+        inorder(root->left);
+        arr.push_back(root->val);
+        inorder(root->right);
+    }
+
+    int next() {
+        return arr[idx++];    
+    }
+    
+    bool hasNext() {
+        if(idx<arr.size())return true;
+        return false;
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
