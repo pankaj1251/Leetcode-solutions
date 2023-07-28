b419796e237a6747435a57376209949414fcbec8
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-07-28 11:02:48 +0530

    Time: 4 ms (63.9%), Space: 8 MB (11%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
 
-    int func(vector<int>& arr, bool flag, int i, int j, int n)
+    int func(vector<int>& arr, bool flag, int i, int j, vector<vector<vector<int>>> &dp)
     {
         if(i>=arr.size() || j<0 || i>j)return 0;
+
+        if(dp[flag][i][j] != -1)return dp[flag][i][j];
         
-        if(flag) return max(arr[i] + func(arr, !flag, i+1, j, n), arr[j] + func(arr, !flag, i, j-1, n));
+        if(flag) return dp[flag][i][j] = max(arr[i] + func(arr, !flag, i+1, j, dp), arr[j] + func(arr, !flag, i, j-1, dp));
         
-        else return min(-arr[i] + func(arr, !flag, i+1, j, n), -arr[j] + func(arr, !flag, i, j-1, n));
+        else return dp[flag][i][j] = min(-arr[i] + func(arr, !flag, i+1, j, dp), -arr[j] + func(arr, !flag, i, j-1, dp));
     }
     bool PredictTheWinner(vector<int>& arr) {
 
         int n = arr.size();
-        int ans = func(arr, true, 0, n-1, n);
+        vector<vector<vector<int>>>dp(2, vector<vector<int>>(n, vector<int>(n, -1)));
+        int ans = func(arr, true, 0, n-1, dp);
         
         return ans>=0;
     }
