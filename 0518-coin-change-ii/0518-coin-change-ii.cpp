4a151eabfa6bc0cb3ce2ae0c2aae9ece1aecf10c
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-08-01 18:30:40 +0530

    Time: 4 ms (98.77%), Space: 18.7 MB (23.24%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int func(vector<int> &arr, int k, int idx, vector<vector<int>> &dp){
+        if(k<0)return 0;
+        if(k==0)return 1;
+        if(k>=1 and idx<0)return 0;
+        if(dp[idx][k] != -1)return dp[idx][k];
+        return dp[idx][k] = func(arr, k-arr[idx], idx, dp) + func(arr, k, idx-1, dp);
+    }
+    int change(int amount, vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>>dp(n, vector<int>(amount+1, -1));
+        return func(arr, amount, n-1, dp);
+    }
+};
