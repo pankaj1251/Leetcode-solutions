a99265a8a6cb9f6c61af6824ad17a2949ee96896
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-08-21 11:41:35 +0530

    Time: 14 ms (87.2%), Space: 13.5 MB (57.6%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-
     bool repeatedSubstringPattern(string s) {
-        int n = s.size();
-        
-        for(int i=1; i<=n/2; i++){
-            if(n%i==0){
-                string substr = s.substr(0, i);
-                string dup="";
-                for(int j=0; j<n/i; j++)dup += substr;
-
-                if(dup==s)return true;
-            }
-        }
-
-        return false;
+        string doubled = s + s;
+        string sub = doubled.substr(1, doubled.size() - 2);
+        return sub.find(s) != string::npos;
     }
 };
