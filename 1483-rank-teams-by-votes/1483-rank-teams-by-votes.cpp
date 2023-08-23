c40e5652df9dddcd490f5c61383f2d3c098e63ee
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-08-23 18:45:36 +0530

    Time: 11 ms (85.02%), Space: 10.8 MB (59.55%) - LeetHub

---
+++ 1483-rank-teams-by-votes/1483-rank-teams-by-votes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    static bool comparator(pair<vector<int>, char>&a, pair<vector<int>, char>&b){
+        if(a.first==b.first)return a.second<b.second;
+        return a.first>b.first;
+    }
+    string rankTeams(vector<string>& votes) {
+        int n = votes.size();
+        int sz = votes[0].size();
+        vector<vector<int>>umap(26, vector<int>(sz, 0));
+        for(auto &voter: votes){
+            for(int i=0; i<voter.size(); i++){
+                umap[voter[i]-'A'][i]++;
+            }
+        }
+
+        vector<pair<vector<int>, char>> table(26);
+
+        for(int i=0; i<26; i++){
+            table[i] = {umap[i], (i+'A')};
+        }
+
+        sort(table.begin(), table.end(), comparator);
+
+        string ans="";
+
+        for(int i=0; i<min(sz, 26); i++){
+            ans += table[i].second;
+        }
+
+        return ans;
+    }
+};
