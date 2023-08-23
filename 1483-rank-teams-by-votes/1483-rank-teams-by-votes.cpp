addd82c9feeaf37b06cc53ba08d6ee2511e01dc2
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-08-23 19:04:10 +0530

    Time: 15 ms (58.8%), Space: 10.8 MB (59.55%) - LeetHub

--- 1483-rank-teams-by-votes/1483-rank-teams-by-votes.cpp
+++ 1483-rank-teams-by-votes/1483-rank-teams-by-votes.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
+
     static bool comparator(pair<vector<int>, char>&a, pair<vector<int>, char>&b){
         if(a.first==b.first)return a.second<b.second;
         return a.first>b.first;
     }
+
     string rankTeams(vector<string>& votes) {
         int n = votes.size();
         int sz = votes[0].size();
@@ -13,6 +15,13 @@ public:
                 umap[voter[i]-'A'][i]++;
             }
         }
+
+        // for(int i=0; i<26; i++){
+        //     for(int j=0; j<umap[i].size(); j++){
+        //         cout<<umap[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
 
         vector<pair<vector<int>, char>> table(26);
 
