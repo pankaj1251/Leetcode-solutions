3be5f4396e7670861595e1b5ce678d7ad411feb8
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-08-26 15:39:53 +0530

    Time: 86 ms (41.22%), Space: 10.2 MB (83.05%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    int func(vector<int>& arr, int st, int en, int arr_start, int arr_end, vector<vector<int>>&dp)
-    {
-        if(arr_start > arr_end)return 0;
-
-        if(dp[arr_start][arr_end] != -1)return dp[arr_start][arr_end];
-
-        int minimum = INT_MAX;
-
-        for(int i = arr_start; i <= arr_end; i++)
-        {
-            minimum = min(minimum, (en-st) + func(arr, st, arr[i], arr_start, i-1, dp) + 
-                                             func(arr, arr[i], en, i+1, arr_end, dp));
+    int func(vector<int>&arr, int start, int end, int i, int j, vector<vector<int>> &dp){
+        if(i>j)return 0;
+        if(dp[i][j] != -1)return dp[i][j];
+        int ans=INT_MAX;
+        for(int idx=i; idx<=j; idx++){
+            ans = min(ans, (end-start) + func(arr, start, arr[idx], i, idx-1, dp) + func(arr, arr[idx], end, idx+1, j, dp));
         }
 
-        return dp[arr_start][arr_end] = minimum;
+        return dp[i][j] = ans;
     }
-    
-    int minCost(int n, vector<int>& arr) 
-    {
-        int sz = arr.size();
-        //If all elements are easily sorted, then find the cuts from the st to the en.
-        //Otherwise, if the vector is not sorted, traversing the entire vector becomes a time-consuming task, which is wasteful.
+    int minCost(int n, vector<int>& arr) {
+        int len = arr.size();
         sort(arr.begin(), arr.end());
-        vector<vector<int>>dp(sz, vector<int>(sz, -1));
-        int ans;
-        ans = func(arr, 0, n, 0, sz-1, dp);
-
-        return ans;
+        vector<vector<int>>dp(len, vector<int>(len, -1));
+        return func(arr, 0, n, 0, len-1, dp);
     }
 };
