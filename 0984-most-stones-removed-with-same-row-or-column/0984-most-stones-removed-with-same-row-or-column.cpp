f0b365915be791647549740d3d62743108c47031
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-09-05 19:44:20 +0530

    Time: 217 ms (25.18%), Space: 14.2 MB (84.82%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-/*
-We can derive from the statement that atleast 1 stone will be left on the plane.
-So, we can just find the number of disconnected groups using BFS.
-For every group, 1 stone will not be picked up.
-Then,
-Stones picked = Total number of stones - Stones left
-*/
-    int removeStones(vector<vector<int>>& arr) {
-        int n = arr.size();
-       
-        int ans=0;
-        vector<int>vis(n, 0);
-        queue<int>Q;
-
-        for(int idx=0; idx<n; idx++){
-            if(!vis[idx]){
-                Q.push(idx);
-                vis[idx]=1;
-                ans++;
-                while(!Q.empty()){
-                    int node = Q.front();
-                    Q.pop();
-
-                    for(int i=0; i<n; i++){
-                        if(!vis[i] and (arr[node][0]==arr[i][0] or arr[node][1]==arr[i][1])){
-                            vis[i]=1;
-                            Q.push(i);
-                        }
-                    }
-                }
+    int n;
+    vector<int> vis;
+    void dfs(int idx,vector<vector<int>> &stones){
+        vis[idx] = true;
+        for(int i=0;i<n;i++){
+            if(vis[i]) continue;
+            if(stones[idx][0] == stones[i][0] || stones[idx][1] == stones[i][1]){
+                dfs(i,stones);
             }
         }
-
-        return n-ans;
+        return;
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        n = stones.size();
+        vis.resize(n,0);
+        int val = 0;
+        for(int i=0;i<n;i++){
+            if(vis[i]) continue;
+            val++;
+            dfs(i,stones);
+        }
+        return n - val;
     }
 };
