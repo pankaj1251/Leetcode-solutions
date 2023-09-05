8a8dad424fbab9e1752d5f35b71b0b1e9e3bce40
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-09-05 16:17:51 +0530

    Time: 72 ms (79.9%), Space: 34.1 MB (67.17%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,34 +1,99 @@
-class Solution {
-public:
-    void dfs(unordered_map<string, vector<string>>&adj, string &email, unordered_set<string> &vis, vector<vector<string>> &ans){
-        vis.insert(email);
-        ans.back().push_back(email);
-        for(auto &adjEmail: adj[email]){
-            if(vis.find(adjEmail) == vis.end()){
-                dfs(adj, adjEmail, vis, ans);
-            }
+class DSU{
+    public:
+    vector<int> size, parent, rank;
+    DSU(int n){
+        size.resize(n+1, 1);
+        parent.resize(n+1);
+        rank.resize(n+1, 0);
+
+        for(int i=0; i<=n; i++){
+            parent[i]=i;
         }
     }
 
+    int findPar(int node){
+        if(node==parent[node])return node;
+        return parent[node] = findPar(parent[node]);
+    }
+
+    void UnionR(int u, int v){
+        int u_par = findPar(u);
+        int v_par = findPar(v);
+
+        if(u_par==v_par)return;
+
+        if(rank[u_par]<rank[v_par]){
+            parent[u_par] = v_par;
+        }
+        else if(rank[v_par]<rank[u_par]){
+            parent[v_par] = u_par;
+        }
+        else{
+            parent[v_par] = u_par;
+            rank[u_par]++;
+        }
+    }
+
+    void UnionS(int u, int v){
+        int u_par = findPar(u);
+        int v_par = findPar(v);
+
+        if(u_par==v_par)return;
+
+        if(size[u_par]<size[v_par]){
+            parent[u_par] = v_par;
+            size[v_par]+=size[u_par];
+
+        }
+        else{
+            parent[v_par] = u_par;
+            size[u_par]+=size[v_par];
+        }
+    }
+};
+class Solution {
+public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& arr) {
-        int n = arr.size();      
-        unordered_map<string, vector<string>>adj(n);
-        unordered_set<string>vis;
-        vector<vector<string>>ans;
+        
+        int n = arr.size();
+        DSU ds(n);
+
+        //add components to unionSet
+        unordered_map<string, int>connect_components;
 
         for(int i=0; i<n; i++){
-            for(int j=1; j<arr[i].size()-1; j++){
-                adj[arr[i][j]].push_back(arr[i][j+1]);
-                adj[arr[i][j+1]].push_back(arr[i][j]);
+            for(int j=1; j<arr[i].size(); j++){
+                string mail = arr[i][j];
+                if(connect_components.find(mail) == connect_components.end()){
+                    connect_components[mail]=i;
+                }
+                else{
+                    ds.UnionS(i, connect_components[mail]);
+                }
             }
         }
 
-        for(auto &it: arr){
-            if(vis.find(it[1]) == vis.end()){
-                ans.push_back({it[0]}); 
-                dfs(adj, it[1], vis, ans);
-                sort(ans.back().begin()+1, ans.back().end());
+        //merge mails
+        vector<string>mergedMail[n];
+        for(auto &it: connect_components){
+            string mail = it.first;
+            int node = ds.findPar(it.second);
+            mergedMail[node].push_back(mail);
+        }
+
+        //ans
+        vector<vector<string>>ans;
+
+        for(int i=0; i<n; i++){
+            if(mergedMail[i].size()==0)continue;
+
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string>temp;
+            temp.push_back(arr[i][0]);
+            for(auto &it: mergedMail[i]){
+                temp.push_back(it);
             }
+            ans.push_back(temp);
         }
 
         return ans;
