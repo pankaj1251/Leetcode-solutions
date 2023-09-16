be2ce64e8a136d2a40dd6c911af5107c23c23698
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-09-16 13:57:12 +0530

    Time: 484 ms (8.25%), Space: 54.2 MB (10.71%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    vector<int>x_dir = {-1, 0, 1, 0};
-    vector<int>y_dir = {0, 1, 0, -1};
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>PQ;
-   
-    int minimumEffortPath(vector<vector<int>>& arr) {
-        PQ.push({0, {0,0}});
-        int ans=0;
+    vector<pair<int,int>>dir={{1,0},{-1,0},{0,-1},{0,1}};
+
+    void func(vector<vector<int>>& arr, int i, int j, int mid, vector<vector<int>> &vis){
+        if(vis[i][j]==1)return;
+
+        vis[i][j]=1;
         int n = arr.size(), m=arr[0].size();
-        vector<vector<int>>vis(n, vector<int>(m, 0));
-        while(!PQ.empty()){
-            auto top = PQ.top();
-            PQ.pop();
 
-            int dist = top.first;
-            int x = top.second.first;
-            int y = top.second.second;
+        for(int ii=0; ii<4; ii++){
+            int nx = i + dir[ii].first;
+            int ny = j + dir[ii].second;
 
-            ans = max(ans, dist);
-            vis[x][y]=1;
+            if(nx<0 || ny<0 || nx>=n || ny>=m)continue;
 
-            if(x==n-1 and y==m-1)break;
-            
-            for(int i=0; i<4; i++)
-            {
-                int nx = x + x_dir[i];
-                int ny = y + y_dir[i];
-
-                if(nx>=0 and ny>=0 and nx<n and ny<m and !vis[nx][ny])
-                    PQ.push({abs(arr[x][y]-arr[nx][ny]), {nx, ny}});
+            if(abs(arr[i][j]-arr[nx][ny]) <= mid){
+                func(arr, nx, ny, mid, vis);
             }
         }
-        return ans;
     }
+    int minimumEffortPath(vector<vector<int>>& arr) {
+        int n = arr.size(), m = arr[0].size();
 
+        int l=0, r=1e9+2;
+
+        while(l<r){
+            int mid = l + (r-l)/2;
+
+            vector<vector<int>>vis(n, vector<int>(m, 0));
+
+            func(arr, 0, 0, mid, vis);
+
+            if(vis[n-1][m-1]==1) r=mid;
+            else l = mid + 1;
+        }
+
+        return l;
+    }
 };
