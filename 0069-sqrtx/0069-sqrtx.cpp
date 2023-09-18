110085f3d3b454982c6e6796d9bcd45ccbaeeaa4
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-09-18 21:17:45 +0530

    Time: 2 ms (52.48%), Space: 6.3 MB (17.29%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0)return x;
+
+        int l=1, h=x;
+
+        while(l<=h){
+            int mid = l+(h-l)/2;
+
+            if(mid==x/mid)return mid;
+            else if(mid < x/mid) l=mid+1;
+            else h=mid-1;
+        }
+        return h;
+    }
+};
