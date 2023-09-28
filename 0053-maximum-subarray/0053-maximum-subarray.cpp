44a33580425b99ec539183c46f031c63927a35a1
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-09-28 11:49:29 +0530

    Time: 93 ms (13.93%), Space: 73.2 MB (5.26%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
+    vector<int>pre, suf;
     int func(vector<int>&arr, int L, int R){
-        if(L>R)return INT_MIN;
+        if(L==R)return arr[L];
         int mid = (L+R)/2;
-        int leftSum=0, currSum=0, rightSum=0;
 
-        for(int i=mid-1; i>=L; i--){
-            currSum += arr[i];
-            leftSum = max(leftSum, currSum);
-        }
-
-        currSum=0;
-        for(int i=mid+1; i<=R; i++){
-            currSum += arr[i];
-            rightSum = max(rightSum, currSum);
-        }
-
-        return max({func(arr, L, mid-1), func(arr, mid+1, R), leftSum + arr[mid] + rightSum});
+        return max({func(arr, L, mid), func(arr, mid+1, R), pre[mid] + suf[mid+1]});
     }
     int maxSubArray(vector<int>& arr) {
         int n = arr.size();
+        pre=suf=arr;
+
+        for(int i=1; i<n; i++){
+            pre[i] += max(0, pre[i-1]);
+        }
+        for(int i=n-2; i>=0; i--){
+            suf[i] += max(0, suf[i+1]);
+        }
+
         return func(arr, 0, n-1);
     }   
 };
