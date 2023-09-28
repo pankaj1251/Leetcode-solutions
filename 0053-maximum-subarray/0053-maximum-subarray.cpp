3c1c145bbef658740250451500b727f2aeacf0ba
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-09-28 11:44:01 +0530

    Time: 115 ms (5.29%), Space: 68.3 MB (7.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& arr) {
-        int n = arr.size();
-        int sum=0, maxsum=INT_MIN;
-        for(int i=0; i<n; i++)
-        {
-            sum += arr[i];
-            maxsum = max(maxsum, sum);
-            if(sum<0)sum=0;
+    int func(vector<int>&arr, int L, int R){
+        if(L>R)return INT_MIN;
+        int mid = (L+R)/2;
+        int leftSum=0, currSum=0, rightSum=0;
+
+        for(int i=mid-1; i>=L; i--){
+            currSum += arr[i];
+            leftSum = max(leftSum, currSum);
         }
 
-        return maxsum;
+        currSum=0;
+        for(int i=mid+1; i<=R; i++){
+            currSum += arr[i];
+            rightSum = max(rightSum, currSum);
+        }
+
+        return max({func(arr, L, mid-1), func(arr, mid+1, R), leftSum + arr[mid] + rightSum});
     }
+    int maxSubArray(vector<int>& arr) {
+        int n = arr.size();
+        return func(arr, 0, n-1);
+    }   
 };
