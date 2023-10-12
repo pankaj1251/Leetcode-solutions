0595f17c2db156a5d96a1839bdee9435e96cd8e2
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-10-12 16:27:30 +0530

    Time: 240 ms (71.14%), Space: 82.4 MB (80.51%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-        vector<int> sortedArrivalTimes(people.begin(), people.end());
-        sort(sortedArrivalTimes.begin(), sortedArrivalTimes.end());
+        vector<int>start, end;
 
-        // Sort the flower intervals based on their start times.
-        sort(flowers.begin(), flowers.end()) ;
-        
-        unordered_map<int, int> bloomCounts;  // Map to store counts of flowers in bloom for each arrival time.
-        priority_queue<int, vector<int>, greater<int>> bloomEndTimes;  // Min heap to track flower bloom end times.
-
-        int flowerIdx = 0 ;
-        for(int personTime: sortedArrivalTimes){
-            // Add flowers that are in bloom at the person's arrival time.
-            while(flowerIdx < flowers.size() && flowers[flowerIdx][0] <= personTime)
-                bloomEndTimes.push(flowers[flowerIdx++][1]);
-
-            // Remove flowers that are no longer in bloom.
-            while(!bloomEndTimes.empty() && bloomEndTimes.top() < personTime)
-                bloomEndTimes.pop();
-
-            // Store the count of flowers in bloom for the person's arrival time.
-            bloomCounts[personTime] = bloomEndTimes.size();
+        for(auto &it: flowers){
+            start.push_back(it[0]);
+            end.push_back(it[1]);
         }
 
-        vector<int> flowerCounts;  // Resulting array to store flower counts for each person.
-        for(int arrivalTime: people)
-            flowerCounts.push_back(bloomCounts[arrivalTime]);
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
 
-        return flowerCounts;  // Return the counts of flowers in bloom for each person's arrival time.
+        vector<int>ans;
+
+        for(auto &t: people){
+            int st = upper_bound(start.begin(), start.end(), t)-start.begin();
+            int en = lower_bound(end.begin(), end.end(), t)-end.begin();
+
+            ans.push_back(st-en);
+        }
+
+        return ans;
     }
 };
