59414046d3c3a0b50c6f879535ba9ff5a571c9da
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-11-01 10:39:03 +0530

    Time: 11 ms (90.7%), Space: 24.3 MB (91.46%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -1,30 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void inorderTraversal(TreeNode* root, unordered_map<int, int> &mp){
-        if(root == NULL) return;                                                   // inorder traversal
-        
-        inorderTraversal(root->left, mp);
-        mp[root->val]++;
-        inorderTraversal(root->right, mp);
+    int maxcount=INT_MIN;
+    int currcount=0;
+    TreeNode* prev = NULL;
+    vector<int>ans;
+
+    void inorder(TreeNode* root, bool flag){
+        if(!root)return;
+
+        inorder(root->left, flag);
+
+        if(prev != NULL and prev->val==root->val){
+            currcount++;
+        }
+        else currcount=1;
+
+        if(flag==false){
+            maxcount = max(maxcount, currcount);
+        }
+        else{
+            if(currcount == maxcount)ans.push_back(root->val);
+        }
+
+        prev=root;
+
+        inorder(root->right, flag);
     }
-	
+
     vector<int> findMode(TreeNode* root) {
-        unordered_map<int, int> mp;
-        vector<int> ans;
-        inorderTraversal(root, mp);
-        int maxValue =  INT_MIN;
-		
-        for(auto i: mp){                                         // for loop to find maxValue
-            if(maxValue < i.second){
-                maxValue = i.second;
-            }
-        }
-		
-        for(auto i: mp){                                       // for loop to find element value and push to ans
-            if(i.second == maxValue){
-                ans.push_back(i.first);
-            }
-        }
+        ans.clear();
+
+        if(root==NULL)return ans;
+
+        inorder(root, false);
+
+        prev=NULL;
+        currcount=0;
+
+        inorder(root, true);
+
         return ans;
     }
 };
